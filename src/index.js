8e359f32f866d4a27049164d639b1e6bd13ace15
Mari <demarius.edwards@gmail.com>
2020-03-18 17:18:06 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,61 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+
+
+/* Add basic styling for NavLinks */
+const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'blue',
+  textDecoration: 'none',
+  color: 'white',
+}
+
+/* add the navbar component */
+const Navbar = () => 
+   <div>
+    <NavLink
+      to="/" exact style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}>Home
+      </NavLink>
+      <NavLink
+      to="/about"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >About</NavLink>
+    <NavLink
+      to="/login"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >Login</NavLink>
+     <NavLink
+      to="/signup"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >Sign Up</NavLink>
+    <NavLink
+      to="/messages"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >Messages</NavLink>
+  </div>;
+
 
 const Home = () => {
   return (
@@ -9,7 +65,69 @@ const Home = () => {
   );
 };
 
-ReactDOM.render(
-  <Home />,
+const About = () => {
+  return (
+    <div>
+      <h1>This is my about component!</h1>
+    </div>
+  );
+};
+
+const Login = () => {
+  return (
+    <div  >
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+          <label htmlFor="username">Username</label>
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+          <label htmlFor="password">Password</label>
+        </div>
+        <input type="submit" value="Login" />
+      </form>
+    </div>
+  )
+}
+
+const SignUp = () => {
+  return (
+    <div>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+          <label htmlFor="username">Username</label>
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+          <label htmlFor="password">Password</label>
+        </div>
+        <input type="submit" value="Login" />
+      </form>
+    </div>
+  );
+};
+
+const Messages = () => {
+  return (
+    <div >
+      <form class="ui form"><textarea placeholder="Tell us more" rows="3"></textarea></form>
+
+    </div>
+  )
+}
+
+ReactDOM.render((
+  <Router> 
+    <div>
+      <Navbar/>
+      <Route exact path="/" component={Home}/>
+      <Route exact path="/about" component={About}/>
+      <Route exact path="/login" component={Login}/>
+      <Route exact path="/signup" component={SignUp}/>
+      <Route exact path="/message" component={Messages}/>
+    </div>
+  </Router>),
   document.getElementById('root')
 );
